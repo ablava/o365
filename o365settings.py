c88dfd43bfcfbfb951f8c28d7d422db81e4f6116
Alexandr Ablovatski <ablovatski@gmail.com>
2016-11-10 16:22:55 -0500

    Create o365settings.py

---
+++ o365settings.py
@@ -0,0 +1,21 @@
+"""
+This config file contains constants describing 
+the specific environment where this script is used.
+"""
+
+# Graph API version
+API_VERSION = '1.6'
+# App user CLIENT_ID
+CLIENT_ID = ''
+# App user CLIENT_KEY
+CLIENT_KEY =''
+# o365 domain, e.g. domain.edu
+O365DOMAIN = ''
+# Pattern that identifies student user names, e.g. _
+STUPATTERN = ''
+# License to assign to students, e.g. skuId of STANDARDWOFFPACK_IW_STUDENT
+STULICENSE = ''
+# License to assign to faculty, e.g. skuId of STANDARDWOFFPACK_IW_FACULTY
+EMPLICENSE = ''
+# Disabled plans, e.g. skuId of MCOSTANDARD and EXCHANGE_S_STANDARD
+DISABLEDPLANS = ['', '']
